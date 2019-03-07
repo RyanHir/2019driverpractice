cd3987119f4f872a82c024d6d705a526120976f0
Ryan H <littlekishi@gmail.com>
2019-03-07 17:37:18 -0600

    vsygfouaerzgsouher8digbhdhbliutsrhglhzrelieg

--- super.js
+++ super.js
@@ -9,6 +9,28 @@ function resetTime() {
     currentTimeMil = 0;
     timer.innerHTML = "Timer: "+currentTimeMin+":"+currentTimeSec+"."+currentTimeSec;
 }
+function startTime() {
+    function idk(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
+    }
+
+    min = currentTimeMin * 60;
+    min = min + currentTimeSec;
+}
+
 
 //Initialize
 resetTime();
