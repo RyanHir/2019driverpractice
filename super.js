b1664caff80dec12330d6c4575cdbb5f2e942578
Ryan H <littlekishi@gmail.com>
2019-03-07 18:58:58 -0600

    wflcjaerlivjeivpuizerhgpiuear

--- super.js
+++ super.js
@@ -1,20 +1,18 @@
 var timer = document.getElementById("time");
 var currentTimeMin = 2;
 var currentTimeSec = 30;
-var currentTimeMil = 0
 
 function resetTime() {
     currentTimeMin = 2;
     currentTimeSec = 30;
-    currentTimeMil = 0;
-    timer.innerHTML = "Timer: "+currentTimeMin+":"+currentTimeSec+"."+currentTimeMil;
+    timer.innerHTML = currentTimeMin+":"+currentTimeSec;
 }
 function startTime() {
     function idk(duration, display) {
         var timer = duration, minutes, seconds;
         setInterval(function () {
             minutes = parseInt(timer / 60, 10)
-            seconds = parseInt(timer % 60, 10);
+            seconds = (timer % 60);
 
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -27,9 +25,9 @@ function startTime() {
         }, 1000);
     }
 
-    min = currentTimeMin * 60;
-    min = min + currentTimeSec;
-    min = min + (currentTimeMil / 1000)
+    startingTime = Date.now;
+
+
 
     idk(min, timer);
 }
